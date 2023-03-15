7a72a49bfcc6e7c94e969e0cf069756404af7cb6
Jeong SeungHo <teapackage16@gmail.com>
2023-03-15 19:18:33 +0900

    edit design

--- website/views.py
+++ website/views.py
@@ -103,7 +103,7 @@ def comment_edit():
     comment_data = request.form["edit_give"]
 
     col.update_one({'index':index,'pw':pw},{'$set':{'comment':comment_data}})
-    return jsonify({'msg': 'True'})
+    return jsonify({'msg': '수정 완료!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
